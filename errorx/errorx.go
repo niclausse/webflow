05f3f1712e392cd2789903edc1a146f23c13843c
linpeng01 <linpeng01@zuoyebang.com>
2022-06-21 18:39:01 +0800

    add biz_error definition

--- errorx/errorx.go
+++ errorx/errorx.go
@@ -19,25 +19,12 @@ func New(errNo int, errMsg string, details ...string) *ErrorX {
 }
 
 func (e *ErrorX) WithDetails(details ...string) *ErrorX {
-	e.Details = append(e.Details, details...)
-	return e
+	x := &ErrorX{
+		BizNo:   e.BizNo,
+		BizMsg:  e.BizMsg,
+		Details: e.Details,
+	}
+
+	x.Details = append(x.Details, details...)
+	return x
 }
-
-const (
-	NoParamInvalid   = 1  //参数错误
-	NoSystemError    = 2  //服务内部错误
-	NoUserNotLogin   = 3  //用户未登录
-	NoInvalidRequest = 14 //无效请求
-)
-
-var ErrMSG = map[int]string{
-	NoParamInvalid:   "请求参数不合理",
-	NoSystemError:    "服务异常， 请稍后重试",
-	NoUserNotLogin:   "用户未登录，请登录后重试",
-	NoInvalidRequest: "请求无效，请稍后再试",
-}
-
-var (
-	ParamInvalid = New(NoParamInvalid, ErrMSG[NoParamInvalid])
-	SystemError  = New(NoSystemError, ErrMSG[NoSystemError])
-)
